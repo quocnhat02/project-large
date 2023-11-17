7f1c39ffc4acf9020c8db891e9143a15a9c2fb76
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-11-17 18:26:28 +0700

    helemt

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const { default: helmet } = require('helmet');
 const morgan = require('morgan');
 
 const app = express();
 
 // init middlewares
 app.use(morgan('dev'));
+app.use(helmet());
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello, World!');
