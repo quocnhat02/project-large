c697be5431d7f91b106b3af31127b4f0be9ea755
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-03-22 22:25:09 +0700

    error response

---
+++ src/core/error.response.js
@@ -0,0 +1,31 @@
+const { ReasonPhrases, StatusCodes } = require('../constants/httpStatusCode');
+
+class ErrorResponse extends Error {
+  constructor(message, status) {
+    super(message);
+    this.status = status;
+  }
+}
+
+class ConflictRequestError extends ErrorResponse {
+  constructor(
+    message = ReasonPhrases.CONFLICT,
+    statusCode = StatusCodes.CONFLICT
+  ) {
+    super(message, statusCode);
+  }
+}
+
+class BadRequestError extends ErrorResponse {
+  constructor(
+    message = ReasonPhrases.BAD_REQUEST,
+    statusCode = StatusCodes.BAD_REQUEST
+  ) {
+    super(message, statusCode);
+  }
+}
+
+module.exports = {
+  ConflictRequestError,
+  BadRequestError,
+};
