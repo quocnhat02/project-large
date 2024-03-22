7bc7c1ef017546d486251aba52f324ddb8b8b3d9
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-03-22 14:35:13 +0700

    controller access

---
+++ src/controllers/access.controller.js
@@ -0,0 +1,15 @@
+class AccessController {
+  signUp = async (req, res, next) => {
+    try {
+      console.log(`[P]:::signUp:::`, req.body);
+      return res.status(201).json({
+        code: '2001',
+        metadata: { userId: 1 },
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+module.exports = new AccessController();
--- src/routes/access/index.js
+++ src/routes/access/index.js
@@ -1,11 +1,8 @@
 const express = require('express');
+const accessController = require('../../controllers/access.controller');
 const router = express.Router();
 
 // signup
-router.post('/signup', (req, res, next) => {
-  return res.status(200).json({
-    message: 'Hello World',
-  });
-});
+router.post('/shop/signup', accessController.signUp);
 
 module.exports = router;
