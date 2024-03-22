44559c639cc29ba89fe50c261e55692ca682a82e
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-03-22 14:22:01 +0700

    create shop model

---
+++ src/models/shop.model.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const DOCUMENT_NAME = 'Shop';
+const COLLECTION_NAME = 'Shops';
+
+const shopSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      max: 150,
+    },
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['active', 'inactive'],
+      default: 'inactive',
+    },
+    verify: {
+      type: mongoose.Schema.Types.Boolean,
+      default: false,
+    },
+    roles: {
+      type: Array,
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+    collection: COLLECTION_NAME,
+  }
+);
+
+module.exports = mongoose.model(DOCUMENT_NAME, shopSchema);
