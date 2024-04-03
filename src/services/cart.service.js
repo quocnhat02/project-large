57509995c1385565e62506ff9dbe7d7c639f79af
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-04-03 09:49:24 +0700

    cart 2

--- src/services/cart.service.js
+++ src/services/cart.service.js
@@ -1,6 +1,8 @@
 'use strict';
 
+const { NotFoundRequestError } = require('../core/error.response');
 const { cart } = require('../models/cart.model');
+const { findProductQuery } = require('../models/repositories/product.repo');
 
 class CartService {
   /// START REPO CART ///
@@ -49,6 +51,54 @@ class CartService {
     }
 
     return await CartService.updateUserCartQuantity({ userId, product });
+  }
+
+  static async addToCartV2({ userId, product = {} }) {
+    const { productId, quantity, old_quantity } =
+      shop_order_ids[0]?.item_products[0];
+    const foundProduct = await findProductQuery({
+      product_id: productId,
+      unSelect: ['__v', 'product_variations'],
+    });
+    if (!foundProduct) {
+      throw new NotFoundRequestError('Not found product');
+    }
+    if (foundProduct.product_shop.toString() !== shop_order_ids[0]?.shopId) {
+      throw new NotFoundRequestError('Product does not belong to the shop');
+    }
+    if (quantity === 0) {
+    }
+
+    return await CartService.updateUserCartQuantity({
+      userId,
+      product: {
+        productId,
+        quantity: quantity - old_quantity,
+      },
+    });
+  }
+
+  static async deleteUserCart({ userId, productId }) {
+    const query = { cart_userId: userId, cart_state: 'active' },
+      updateSet = {
+        $pull: {
+          cart_products: {
+            productId,
+          },
+        },
+      };
+
+    const deleteCart = await cart.updateOne(query, updateSet);
+
+    return deleteCart;
+  }
+
+  static async getListUserCart({ userId }) {
+    return await cart
+      .findOne({
+        cart_userId: +userId,
+      })
+      .lean();
   }
 }
 
